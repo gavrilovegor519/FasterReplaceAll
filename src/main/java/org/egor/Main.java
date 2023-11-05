bb7f34ede580387ab0714162d7b2e18ef3fb1b9b
Egor Gavrilov <gavrilovegor519@gmail.com>
2023-11-06 00:27:00 +0300

    Delete Main class (because I'm using unit tests).

--- src/main/java/org/egor/Main.java
+++
@@ -1,9 +0,0 @@
-package org.egor;
-
-public class Main {
-    public static void main(String[] args) {
-        String string1 = "\"[]\"\n\"[]\"\n";
-
-        System.out.println(ReplaceAll.replaceAll(string1));
-    }
-}
